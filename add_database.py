188acc102fc16f0ebb5cd98290c8142f02e4c1b7
Joseph Dorris <jdorris1331@gmail.com>
2016-06-22 14:57:25 -0400

    updated database fill script

--- add_database.py
+++ add_database.py
@@ -6,24 +6,48 @@ db = MySQLdb.connect('localhost','joe','password','patents');
 
 cursor = db.cursor()
 
-for i in range(0,998):
+for i in range(0,999):
   print i
-  file = "xx{0:03d}".format(i)
+  file = "bulk_data/xx{0:04d}".format(i)
   tree = ET.parse(file)
   root = tree.getroot()
 
   #cursor.execute("SELECT * FROM patent_info")
 
+
   #ID = root[0][0][0][1].text
-  ID = root.find('us-bibliographic-data-grant').find('publication-reference').find('document-id').find('doc-number').text
+  if root.find('us-bibliographic-data-grant') is not None:
+    bib = root.find('us-bibliographic-data-grant')
+  else:
+    continue
+  
+  ID = bib.find('publication-reference').find('document-id').find('doc-number').text
+  # some dont have us-bibliographic-data-grant
+
   #title = root[0][6].text
-  title = root.find('us-bibliographic-data-grant').find('invention-title').text
+
+
+  title = "NULL"
+  if bib.find('invention-title') is not None:
+    if bib.find('invention-title').text is not None:
+      title = bib.find('invention-title').text.encode('ascii','ignore')
+
+  title = title.replace("'","")
+
   temp_date = root.attrib['date-publ']
   date = temp_date[0:4] + "-" + temp_date[4:6] + "-" + temp_date[6:8]
   inventor = "NULL"
-  #inventor = root.find('us-bibliographic-data-grant').find('us-parties').find('inventors').find('inventor').find('addressbook').find('first-name').text + " " + root.find('us-bibliographic-data-grant').find('us-parties').find('inventors').find('inventor').find('addressbook').find('last-name').text
+  inventor = bib.find('us-parties').find('inventors').find('inventor').find('addressbook').find('first-name').text + " " + root.find('us-bibliographic-data-grant').find('us-parties').find('inventors').find('inventor').find('addressbook').find('last-name').text
+  inventor = inventor.replace("'","").encode('ascii','ignore')
+
   assignee = "NULL"
-  #assignee = root.find('us-bibliographic-data-grant').find('assignees').find('assignee').find('addressbook').find('orgname').text
+  if bib.find('assignees') is not None:
+    if bib.find('assignees').find('assignee').find('addressbook').find('orgname') is not None:
+      assignee = bib.find('assignees').find('assignee').find('addressbook').find('orgname').text
+    elif bib.find('assignees').find('assignee').find('addressbook').find('last-name') is not None:
+      assignee = bib.find('assignees').find('assignee').find('addressbook').find('first-name').text + " " + bib.find('assignees').find('assignee').find('addressbook').find('last-name').text
+  assignee = assignee.replace("'","").encode('ascii','ignore')
+  
   ref = "NULL"
   #for j in range(0,len(root.find('us-bibliographic-data-grant').find('us-references-cited'))):
   #  ref += [root.find('us-bibliographic-data-grant').find('us-references-cited')[j].find('patcit').find('document-id').find('doc-number').text]  
@@ -35,11 +59,23 @@ for i in range(0,998):
   cursor.execute(query)
   db.commit()
 
+
+  #for j in range(0,len(root.find('claims'))):
+    #root.find('claims')[j].find('claim').find('claim-text').text
+
+  #abstract 
+
+  #description
+
+  #images
+
 db.close()
 
 #INSERT INTO patent_test VALUES ('7956546', 'Modular LED light bulb', '2011-06-07', 'Ghulam Hasnain', 'Bridgelux, Inc.', '[6127783, 6596977, 7215086, 7458934, 7479662, 7524097, 7641364, 20040066142, 20040222516, 20060097245, 20060227558, 20070109782]','[8306639, 8350485, 8421376, 8422889, 8430402, 8531137, 8708525]');
 
 #patent_data = ID, claims, abstract, description, images
 #  data = cursor.fetchone()
+
+
 
 #json.loads(data[5])
---
+++ back_connect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in table:
+SELECT ref from i
+ 
+  for j in ref
+    append i to j ref_by
