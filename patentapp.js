ce87e01875e37c1efd3278d630c7ddf753286ac5
Joseph Dorris <jdorris1331@gmail.com>
2016-06-24 10:42:11 -0400

    fixed added nodes and edges

--- patentapp.js
+++ patentapp.js
@@ -31,7 +31,22 @@ app.get('/search', function (req, res) {
           throw err;
         }
         else {
-          res.render('show_patent',{ title: req.query.patent_num, author: rows[0].inventor, abstract: rows[0].abstract, claims: rows[0].claims });
+      var nodes = [
+        {id: 1, label: 'US7322199', title: 'Stirling cooler'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'} 
+      ];   
+  
+      var edges = [
+        {from: 1, to: 3, title: '1927'},
+        {from: 1, to: 2, color:{color:'red'}},
+        {from: 2, to: 4, color:'rgb(20,24,200)'},  
+        {from: 2, to: 5}
+      ]; 
+      //var data_json = JSON.stringify(data);
+          res.render('show_patent',{ title: req.query.patent_num, author: rows[0].inventor, abstract: rows[0].abstract, claims: rows[0].claims, nodes: nodes, edges: edges });
         }
       });
     }
--- scripts/testscript.js
+++ scripts/testscript.js
@@ -1,26 +1,4 @@
-//<script type="text/javascript">
-  //var nodes = new vis.DataSet([
-  //  {id: 1, label: 'US7322199', title: 'Stirling cooler'},
-  //  {id: 2, label: 'Node 2'},
-  //  {id: 3, label: 'Node 3'},
-  //  {id: 4, label: 'Node 4'},
-  //  {id: 5, label: 'Node 5'} 
-  //]);   
-
-  // create an array with edges
-  //var edges = new vis.DataSet([
-  //  {from: 1, to: 3, title: '1927'},
-  //  {from: 1, to: 2, color:{color:'red'}},
-  //  {from: 2, to: 4, color:'rgb(20,24,200)'},  
-  //  {from: 2, to: 5}
-  //]);   
-
-  // create a network
+  var data = JSON.parse(data_json);
   var container = document.getElementById('mynetwork');
-  var data = { 
-    nodes: nodes,
-    edges: edges
-  };    
   var options = {};
   var network = new vis.Network(container, data, options);
-</script>
