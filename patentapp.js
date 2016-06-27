99318188920cc5ef2a85e8c5bb3d9a1a1648bed7
Joseph Dorris <jdorris1331@gmail.com>
2016-06-27 12:44:05 -0400

    build web from references

--- patentapp.js
+++ patentapp.js
@@ -26,25 +26,35 @@ app.get('/search', function (req, res) {
     if (err) throw err;
     if(rows[0]['count(1)']==1) {
       //connection.query('SELECT inventor FROM patent_info WHERE ID = ? ', [req.query.patent_num], function(err, rows, fields) {
-      connection.query('SELECT patent_test.inventor, patent_data_test.abstract, patent_data_test.claims FROM patent_test INNER JOIN patent_data_test ON patent_test.ID=patent_data_test.ID WHERE patent_test.ID=? ', [req.query.patent_num], function(err, rows, fields) {
+      connection.query('SELECT patent_test.inventor, patent_test.ref, patent_data_test.abstract, patent_data_test.claims FROM patent_test INNER JOIN patent_data_test ON patent_test.ID=patent_data_test.ID WHERE patent_test.ID=? ', [req.query.patent_num], function(err, rows, fields) {
         if (err) {
           throw err;
         }
         else {
-      var nodes = [
-        {id: 1, label: 'US7322199', title: 'Stirling cooler'},
-        {id: 2, label: 'Node 2'},
-        {id: 3, label: 'Node 3'},
-        {id: 4, label: 'Node 4'},
-        {id: 5, label: 'Node 5'} 
-      ];   
-  
-      var edges = [
-        {from: 1, to: 3, title: '1927'},
-        {from: 1, to: 2, color:{color:'red'}},
-        {from: 2, to: 4, color:'rgb(20,24,200)'},  
-        {from: 2, to: 5}
-      ]; 
+          var ref = JSON.parse(rows[0].ref);
+          var nodes = [];
+          nodes.push({id: 1, label: req.query.patent_num});
+          var edges = [];
+          for (i = 0; i < ref.length; i++) {
+            console.log(ref[i]);
+            nodes.push({id: i+2, label: ref[i]});
+            edges.push({from: i+2, to: 1});
+          } 
+          console.log(rows[0].ref);
+          /*var nodes = [
+            {id: 1, label: 'US7322199', title: 'Stirling cooler'},
+            {id: 2, label: 'Node 2'},
+            {id: 3, label: 'Node 3'},
+            {id: 4, label: 'Node 4'},
+            {id: 5, label: 'Node 5'} 
+          ];   
+      
+          var edges = [
+            {from: 1, to: 3, title: '1927'},
+            {from: 1, to: 2, color:{color:'red'}},
+            {from: 2, to: 4, color:'rgb(20,24,200)'},  
+            {from: 2, to: 5}
+          ];*/ 
       //var data_json = JSON.stringify(data);
           res.render('show_patent',{ title: req.query.patent_num, author: rows[0].inventor, abstract: rows[0].abstract, claims: rows[0].claims, nodes: nodes, edges: edges });
         }
