5e0dcf331818905ac7fb1b2afa42d75a3673c439
Joseph Dorris <jdorris1331@gmail.com>
2016-07-12 11:51:55 -0400

    show title

--- patentapp.js
+++ patentapp.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+function pad (str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
+}
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -21,19 +26,27 @@ app.get('/', function (req, res) {
 });
 
 app.get('/search', function (req, res) {
-  if(/^[0-9]{8}/.test(req.query.patent_num) == true ) {
-  connection.query('SELECT count(1) FROM patent_info WHERE ID = ? ', [req.query.patent_num], function(err, rows, fields) {
+  patent_num = 0;
+  if(/^\d+$/.test(req.query.patent_num) == true ) {
+    patent_num = pad(req.query.patent_num,8);
+    console.log(patent_num);
+  }
+  else if(/^[0-9]{8}/.test(req.query.patent_num) == true ) {
+    patent_num = req.query.patent_num;
+  }
+  if( patent_num != 0 ) {
+  connection.query('SELECT count(1) FROM patent_info WHERE ID = ? ', [patent_num], function(err, rows, fields) {
     if (err) throw err;
     if(rows[0]['count(1)']==1) {
-      //connection.query('SELECT inventor FROM patent_info WHERE ID = ? ', [req.query.patent_num], function(err, rows, fields) {
-      connection.query('SELECT patent_info.inventor, patent_info.ref, patent_data.abstract, patent_data.claims FROM patent_info INNER JOIN patent_data ON patent_info.ID=patent_data.ID WHERE patent_info.ID=? ', [req.query.patent_num], function(err, rows, fields) {
+      //connection.query('SELECT inventor FROM patent_info WHERE ID = ? ', [patent_num], function(err, rows, fields) {
+      connection.query('SELECT patent_info.title, patent_info.inventor, patent_info.ref, patent_data.abstract, patent_data.claims FROM patent_info INNER JOIN patent_data ON patent_info.ID=patent_data.ID WHERE patent_info.ID=? ', [patent_num], function(err, rows, fields) {
         if (err) {
           throw err;
         }
         else {
           var ref = JSON.parse(rows[0].ref);
           var nodes = [];
-          nodes.push({id: 1, label: req.query.patent_num});
+          nodes.push({id: 1, label: patent_num});
           var edges = [];
           for (i = 0; i < ref.length; i++) {
             //console.log(ref[i]);
@@ -55,7 +68,7 @@ app.get('/search', function (req, res) {
             {from: 2, to: 5}
           ];*/ 
       //var data_json = JSON.stringify(data);
-          res.render('show_patent',{ title: req.query.patent_num, author: rows[0].inventor, abstract: rows[0].abstract, claims: rows[0].claims, nodes: nodes, edges: edges });
+          res.render('show_patent',{ title: rows[0].title, id: patent_num, author: rows[0].inventor, abstract: rows[0].abstract, claims: rows[0].claims, nodes: nodes, edges: edges });
         }
       });
     }
